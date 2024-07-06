3325f41809b075b47c712648cf83d968d4b4f14c
mhdmishab <you@example.com>
2024-07-06 21:51:07 +0530

    url updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://mapmee.netlify.app'],
     optionsSuccessStatus: 200 
 };
 
